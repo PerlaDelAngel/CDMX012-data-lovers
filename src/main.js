99b481d0f843febaee08792408db809a716a32f0
Perla Del Ángel <perdelangel@gmail.com>
2022-02-02 18:26:47 -0600

    Update al diseño de la página principal

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+
+import data from './data/ghibli/ghibli.js';
+
+console.log(typeof data);
+
 
 console.log(example, data);
