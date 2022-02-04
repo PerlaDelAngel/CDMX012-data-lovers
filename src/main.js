5a7953479fd283dfe0852625ef95e47e10b9e669
Perla Del Ángel <perdelangel@gmail.com>
2022-02-04 11:15:02 -0600

    Aparecen los títulos de las películas en negritas y los años en una linea aparte

--- src/main.js
+++ src/main.js
@@ -7,23 +7,18 @@ let allMovies = [];
 for(let i = 0; i < data.films.length; i++){
     allMovies.push(example(data.films[i]));
 }
- 
-for(let j = 0; j < allMovies.length; j++){
 
+for(let j = 0; j < allMovies.length; j++){
     let elementArticle = document.createElement("article");
 
     let imgMovie = document.createElement("img");
     imgMovie.setAttribute("src", allMovies[j][0]);
 
     let elementP = document.createElement("p");
-    let elementPP = document.createElement("p");
-
-    let title = document.createTextNode(allMovies[j][1]);
-    let year = document.createTextNode(allMovies[j][2]);
-
+    elementP.innerHTML = "<strong>" + allMovies[j][1] + "</strong>" + "<br>" + allMovies[j][2];
+    
     elementArticle.appendChild(imgMovie);
-    elementArticle.appendChild(elementP.appendChild(title));
-    elementArticle.appendChild(elementPP.appendChild(year));
+    elementArticle.appendChild(elementP);
 
     document.getElementById("movieList").appendChild(elementArticle);
 }
